48deb10d975523b989dc8bc8b833770743de649f
frajova <frajova@gmail.com>
2021-03-14 20:56:26 -0300

    FIX: add login inputs

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import React, { Suspense } from 'react';
 import Routes from './Routes';
 
 const App: React.FC = () => (
-  <div className="h-100" data-test="appComponent">
-    <Suspense fallback={<div className="loading" />}>
-      <Routes />
-    </Suspense>
-  </div>
+  <Suspense fallback={<div className="loading" />}>
+    <Routes />
+  </Suspense>
 );
 
 export default App;
--- src/views/admin/login.tsx
+++ src/views/admin/login.tsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import {
+  Container, Col, Form,
+  FormGroup, Label, Input,
+  Button,
+} from 'reactstrap';
+import styled from 'styled-components';
 
 const Login = () => (
-    <div>
-      Login
-    </div>
-  )
+  <LoginContainer>
+    <Container className="App">
+      <h2>Sign In</h2>
+      <Form className="form">
+        <Col>
+          <FormGroup>
+            <Label>Email</Label>
+            <Input
+              type="email"
+              name="email"
+              id="exampleEmail"
+              placeholder="myemail@email.com"
+            />
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label for="examplePassword">Password</Label>
+            <Input
+              type="password"
+              name="password"
+              id="examplePassword"
+              placeholder="********"
+            />
+          </FormGroup>
+        </Col>
+        <Button>Submit</Button>
+      </Form>
+    </Container>
+  </LoginContainer>
+)
 
 export default Login
+
+const LoginContainer: React.FC = styled.div`
+
+`;
